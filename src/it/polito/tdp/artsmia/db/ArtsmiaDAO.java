65af1b1fffab31570da13a99386aada10521ae1d
aleis <aleis@DESKTOP-EDEJ39O>
2019-06-25 10:45:23 +0200

    ultima prova

--- src/it/polito/tdp/artsmia/db/ArtsmiaDAO.java
+++ src/it/polito/tdp/artsmia/db/ArtsmiaDAO.java
@@ -6,12 +6,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import it.polito.tdp.artsmia.model.Adiacenza;
 import it.polito.tdp.artsmia.model.ArtObject;
 
 public class ArtsmiaDAO {
 
-	public List<ArtObject> listObjects() {
+	public List<ArtObject> listObjects(Map<Integer, ArtObject> idMap) {
 		
 		String sql = "SELECT * from objects";
 		List<ArtObject> result = new ArrayList<>();
@@ -21,6 +23,7 @@ public class ArtsmiaDAO {
 			PreparedStatement st = conn.prepareStatement(sql);
 			ResultSet res = st.executeQuery();
 			while (res.next()) {
+				if(idMap.get(res.getInt("object_id")) == null) {
 
 				ArtObject artObj = new ArtObject(res.getInt("object_id"), res.getString("classification"), res.getString("continent"), 
 						res.getString("country"), res.getInt("curator_approved"), res.getString("dated"), res.getString("department"), 
@@ -28,6 +31,46 @@ public class ArtsmiaDAO {
 						res.getString("rights_type"), res.getString("role"), res.getString("room"), res.getString("style"), res.getString("title"));
 				
 				result.add(artObj);
+				idMap.put(artObj.getId(), artObj);
+				}else {
+					result.add(idMap.get(res.getInt("objext_id")));
+				}
+			}
+			conn.close();
+			return result;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public List<Adiacenza> getAdiacenza(Map<Integer, ArtObject> idMap) {
+		
+		final String sql="select eo1.object_id id1, eo2.object_id id2, count(*) as peso " + 
+				"from exhibition_objects eo1, exhibition_objects eo2 " + 
+				"where eo1.exhibition_id=eo2.exhibition_id " + 
+				"and eo1.object_id > eo2.object_id " + 
+				"group by id1,id2";
+		
+		List<Adiacenza> result= new ArrayList<>();
+		
+		Connection conn = DBConnect.getConnection();
+
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			ResultSet res = st.executeQuery();
+
+			while (res.next()) {
+
+				ArtObject a1 = idMap.get(res.getInt("id1"));
+				ArtObject a2 = idMap.get(res.getInt("id2"));
+				double peso = res.getDouble("peso");
+
+				if (a1 != null && a1 != null) {
+					Adiacenza a = new Adiacenza(a1, a2, peso);
+					result.add(a);
+				}
 			}
 			conn.close();
 			return result;
